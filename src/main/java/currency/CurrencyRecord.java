35e3bc3afdad7ba70fe75a3f508e9bac52252114
Andrii Dibrov <andrii.dibrov@gmail.com>
2021-11-28 13:58:55 +0200

    Optimize CurrencyRecord JSON reading

--- src/main/java/currency/CurrencyRecord.java
+++ src/main/java/currency/CurrencyRecord.java
@@ -3,12 +3,12 @@ package currency;
 import org.json.JSONObject;
 
 public class CurrencyRecord {
-    Currency currencyA;
-    Currency currencyB;
-    int date;
-    double rateBuy;
-    double rateSell;
-    double rateCross;
+    private Currency currencyA;
+    private Currency currencyB;
+    private int date;
+    private double rateBuy;
+    private double rateSell;
+    private double rateCross;
 
     public CurrencyRecord(JSONObject jsonRecord, CurrencyDecoder currencyDecoder) {
         date = (Integer) jsonRecord.get("date");
@@ -17,12 +17,28 @@ public class CurrencyRecord {
         currencyA = currencyDecoder.getCurrency(currencyCodeA);
         currencyB = currencyDecoder.getCurrency(currencyCodeB);
 
-        try {
-            rateCross = Double.parseDouble(jsonRecord.get("rateCross").toString());
+        if(jsonRecord.has("rateBuy"))
             rateBuy = Double.parseDouble(jsonRecord.get("rateBuy").toString());
+
+        if(jsonRecord.has("rateSell"))
             rateSell = Double.parseDouble(jsonRecord.get("rateSell").toString());
-        }
-        catch (Exception e){
-        }
+
+        if(jsonRecord.has("rateCross"))
+            rateCross = Double.parseDouble(jsonRecord.get("rateCross").toString());
+        else
+            rateCross = (rateBuy + rateSell) / 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(currencyA.getAlphabeticCode());
+        sb.append("\t");
+        sb.append(currencyB.getAlphabeticCode());
+        sb.append("\t");
+        sb.append(date);
+        sb.append("\t");
+        sb.append(rateCross);
+        return sb.toString();
     }
 }
