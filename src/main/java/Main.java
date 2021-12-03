7c2d67c7bc9c4470932da4085c1b6ac448b7236b
Andrii Dibrov <andrii.dibrov@gmail.com>
2021-12-03 12:16:12 +0200

    UPD RESTController, now its using Java 11 http

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,22 @@
 import constants.Monobank;
 import currency.CurrencyDecoder;
-import currency.CurrencyRecord;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import rest.RESTController;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
+import java.net.URI;
 
 import com.opencsv.exceptions.CsvException;
+import org.json.JSONArray;
+import rest.RESTController;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CsvException {
+    public static void main(String[] args) throws IOException, CsvException, InterruptedException {
 
         String currencyCodesFilePath = "ISO4217.csv";
+        String merchantCodesFilePath = "ISO18245.csv";
         CurrencyDecoder currencyDecoder = new CurrencyDecoder(currencyCodesFilePath);
 
-        JSONArray jsonArray = RESTController.getJSONArray(new URL(Monobank.CURRENCY));
-
-        ArrayList<CurrencyRecord> currencyRecords = new ArrayList<>();
-        for(int i = 0; i < jsonArray.length(); i++){
-            CurrencyRecord currencyRecord = new CurrencyRecord(jsonArray.getJSONObject(i), currencyDecoder);
-            currencyRecords.add(currencyRecord);
-
-            System.out.println(currencyRecord.toString());
-        }
+        JSONArray ja = RESTController.getJSONArray(URI.create(Monobank.CURRENCY));
 
         System.out.println();
     }
+
 }
--- src/main/java/rest/RESTController.java
+++ src/main/java/rest/RESTController.java
@@ -3,56 +3,39 @@ package rest;
 import Utils.Delay;
 import org.json.*;
 import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class RESTController {
 
     private static final int CONNECTION_TRIES = 3;
     private static final int WAITING_TIME_SEC = 30;
 
-    private static String getJsonString(URL link){
+    public static JSONObject getJSON(URI link) {
+        return new JSONObject(getString(link));
+    }
 
+    public static JSONArray getJSONArray(URI link) {
+        return new JSONArray(getString(link));
+    }
+
+    private static String getString(URI link){
         for(int i = 0; i < CONNECTION_TRIES; i++) {
             try {
-
-                InputStream inputStream = link.openStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-                BufferedReader re = new BufferedReader(inputStreamReader);
-                String jsonString = Read(re);
-                inputStream.close();
-                return jsonString;
-
-            } catch (IOException e) {
-
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder(link).build();
+                HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                return response.body().toString();
+            } catch (IOException | InterruptedException e) {
                 System.out.println("EXCEPTION: couldn't open stream. Reason: " + e.getMessage());
-
                 if (e.getMessage().contains("Server returned HTTP response code: 429"))
                     System.out.println("Too many requests.");
-
-                System.out.println("Reconnect ["+ (i+1) + "/" + CONNECTION_TRIES + "] in" + WAITING_TIME_SEC + " seconds...");
+                System.out.println("Reconnect [" + (i + 1) + "/" + CONNECTION_TRIES + "] in" + WAITING_TIME_SEC + " seconds...");
                 Delay.waitSeconds(WAITING_TIME_SEC);
             }
         }
         return null;
-    }
-
-    public static JSONObject getJSON(URL link) {
-        return new JSONObject(getJsonString(link));
-    }
-
-    public static JSONArray getJSONArray(URL link) {
-        return new JSONArray(getJsonString(link));
-    }
-
-    static String Read(Reader re) throws IOException {
-        StringBuilder str = new StringBuilder();
-        int temp;
-        do {
-            temp = re.read();
-            str.append((char) temp);
-
-        } while (temp != -1);
-        return str.toString();
     }
 }
